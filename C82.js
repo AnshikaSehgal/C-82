8830e77890e6f41b66aa8f877f4065d585464a11
AnshikaSehgal <100148967+AnshikaSehgal@users.noreply.github.com>
2022-05-08 13:44:39 +0530

    Add files via upload

---
+++ C82.js
@@ -0,0 +1,26 @@
+console.log("My first console project");
+
+            var first_name = "Ranbir";
+            var last_name = "Kapoor";
+
+
+            var full_name =  ;
+            console.log(full_name);
+
+            var a = 10;
+            var b = 5;
+
+            var add =   ;
+            console.log( );
+
+
+            var sub =   ;
+            console.log( );
+
+
+            var multiple =   ;
+            console.log();
+
+            var divide = a / b ;
+            console.log(divide);
+
